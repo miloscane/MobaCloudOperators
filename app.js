8028dff175fa6ddfad1df7466b01d2e693e4f9aa
MilosIvankovicEon <milos.ivankovic@mobatec.nl>
2024-04-03 17:09:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ http.listen(process.env.PORT, function(){
 	})
 });
 
-server.get('/modeller/:url1/url2',async (req,res)=>{
+server.get('/modeller/:url1/:url2',async (req,res)=>{
 	res.render("home",{
 		url1:decodeURIComponent(req.params.url1),
 		url2:decodeURIComponent(req.params.url2),
