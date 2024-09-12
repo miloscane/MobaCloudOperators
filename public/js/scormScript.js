25c536fadb52c6af1e6e8fce4b08042d3affd7ac
MilosIvankovicEon <milos.ivankovic@mobatec.nl>
2024-09-12 11:25:07 +0200

    a

--- public/js/scormScript.js
+++ public/js/scormScript.js
@@ -1,14 +1,11 @@
 console.log("Loaded scorm script v1.6");
 
 setInterval(function(){
-	var simulationButtons = document.getElementsByClassName("noOutline");
-	console.log("All noOutline divs");
-	console.log(allImages)
-	for(var i=0;i<allImages.length;i++){
-		console.log(allImages[i].alt)
-		if(allImages[i].alt.startsWith("MobaCloudOpC")){
+	var elems = document.getElementById("content-frame").contentWindow.document.getElementsByTagName("IMG");
+	for(var i=0;i<elems.length;i++){
+		if(elems[i].alt.startsWith("MobaCloudOpC")){
 			console.log("found an image")
-			allImages[i].setAttribute("onclick","console.log('you found the simulation button')");
+			elems[i].setAttribute("onclick","console.log('you found the simulation button')");
 
 		}
 	}	
