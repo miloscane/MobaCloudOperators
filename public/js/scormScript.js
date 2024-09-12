729ddd6c51e3edd9e194752d2974467d8a0cd0f0
MilosIvankovicEon <milos.ivankovic@mobatec.nl>
2024-09-12 11:12:53 +0200

    Update scormScript.js

--- public/js/scormScript.js
+++ public/js/scormScript.js
@@ -1,13 +1,15 @@
 console.log("Loaded scorm script v1.5");
 
-var allImages = document.getElementsByTagName("IMG");
-console.log("All image");
-console.log(allImages)
-for(var i=0;i<allImages.length;i++){
-	console.log(allImages[i].alt)
-	if(allImages[i].alt.startsWith("MobaCloudOpC")){
-		console.log("found an image")
-		allImages[i].setAttribute("onclick","console.log('you found the simulation button')");
+setInterval(function(){
+	var allImages = document.getElementsByTagName("IMG");
+	console.log("All image");
+	console.log(allImages)
+	for(var i=0;i<allImages.length;i++){
+		console.log(allImages[i].alt)
+		if(allImages[i].alt.startsWith("MobaCloudOpC")){
+			console.log("found an image")
+			allImages[i].setAttribute("onclick","console.log('you found the simulation button')");
 
-	}
-}
+		}
+	}	
+},1000)
