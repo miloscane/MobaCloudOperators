84f0d7144b79918495cdb2f748165af4480856a9
MilosIvankovicEon <milos.ivankovic@mobatec.nl>
2024-09-17 21:14:57 +0200

    Update scormScript.js

--- public/js/scormScript.js
+++ public/js/scormScript.js
@@ -1,4 +1,4 @@
-console.log("Loaded scorm script v2.20");
+console.log("Loaded scorm script v2.21");
 
 function loadMobaCloudModel(model){
 	var mobacloudIframe = document.getElementById("mobacloud");
