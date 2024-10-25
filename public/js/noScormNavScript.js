808489d06350a0ffcf02e87eca79da30dd7a5cae
MilosIvankovicEon <milos.ivankovic@mobatec.nl>
2024-10-25 14:06:43 +0200

    Update noScormNavScript.js

--- public/js/noScormNavScript.js
+++ public/js/noScormNavScript.js
@@ -1,6 +1,9 @@
-console.log("NO SCORM NAVIGATION SCRIPT v1.2");
+console.log("NO SCORM NAVIGATION SCRIPT v1.3");
 var studentIDUrl = window.location.href.split("studentID=")[0];
 var liElems = document.getElementsByTagName("NAV")[0].getElementsByTagName("LI");
 for(var i=0;i<liElems.length;i++){
-	liElems[i].getElementsByTagName("A")[0].src = liElems[i].getElementsByTagName("A")[0].src + "?studentID="+studentIDUrl
+	liElems[i].getElementsByTagName("A")[0].href = liElems[i].getElementsByTagName("A")[0].href + "?studentID="+studentIDUrl;
+	console.log("Altered link");
 }
+
+console.log("Active student: "+ studentIDUrl)
