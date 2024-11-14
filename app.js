f3e96a9bdd85a0f2a80aa1dee556d53b1ce1d4c1
MilosIvankovicEon <milos.ivankovic@mobatec.nl>
2024-11-14 10:13:56 +0100

    a

--- app.js
+++ app.js
@@ -215,7 +215,7 @@ server.get('/teacher',async (req,res)=>{
 	});
 });
 
-/*axios.post('https://student.instances.modeller.cloud/stop', new URLSearchParams({uuid: "ABCDABCDABCDABC1"}))
+/*axios.post('https://student.instances.modeller.cloud/stop', new URLSearchParams({uuid: "QNCLYWUEKJEVXTSU"}))
 .then((dockerResponse)=>{
 	//console.log(response);
 	//res.redirect(users[0].url+"&modelpath="+req.query.modelpath)
@@ -230,18 +230,46 @@ server.get('/lmsLogin/:hostname/:lmsid',async (req,res)=>{
 	lmsUsersDB.find({hostname:decodeURIComponent(req.params.hostname),lmsid:decodeURIComponent(req.params.lmsid)}).toArray()
 	.then((users)=>{
 		if(users.length>0){
-			res.redirect(users[0].url+"&modelpath="+req.query.modelpath)
-				//console.log(users[0].code)
-			/*axios.post('https://student.instances.modeller.cloud/start', new URLSearchParams({uuid: users[0].code}))
+			//res.redirect(users[0].url+"&modelpath="+req.query.modelpath)
+			axios.post('https://student.instances.modeller.cloud/start', new URLSearchParams({uuid: users[0].code}))
 			.then((dockerResponse)=>{
 				//console.log(response);
 				//res.redirect(users[0].url+"&modelpath="+req.query.modelpath)
-				res.redirect("https://student.instances.modeller.cloud/connect/"+users[0].code+"/vnc.html?path=connect/"+users[0].code+"/websocketify&password=7b0ce21a0d8d3c7adec51d48abe2a3e9&autoconnect=true&reconnect=true&modelpath="+req.query.modelpath)
+				axios.get("https://student.instances.modeller.cloud/connect/"+users[0].code+"/vnc.html?path=connect/"+users[0].code+"/websocketify&password=7b0ce21a0d8d3c7adec51d48abe2a3e9&autoconnect=true&reconnect=true&modelpath="+req.query.modelpath)
+				.then((response)=>{
+					if(response.status==200){
+						res.redirect("https://student.instances.modeller.cloud/connect/"+users[0].code+"/vnc.html?path=connect/"+users[0].code+"/websocketify&password=7b0ce21a0d8d3c7adec51d48abe2a3e9&autoconnect=true&reconnect=true&modelpath="+req.query.modelpath)
+					}else{
+						res.render("reload-container",{
+							bucket: bucket
+						})
+					}
+
+				})
+				.catch((error)=>{
+					res.render("reload-container",{
+						bucket: bucket
+					})
+				})
 				//console.log("Redirected to: "+users[0].url+"&modelpath="+req.query.modelpath);
 			})
 			.catch((error)=>{
 				if(error.status==409){
-					res.redirect("https://student.instances.modeller.cloud/connect/"+users[0].code+"/vnc.html?path=connect/"+users[0].code+"/websocketify&password=7b0ce21a0d8d3c7adec51d48abe2a3e9&autoconnect=true&reconnect=true&modelpath="+req.query.modelpath)
+					axios.get("https://student.instances.modeller.cloud/connect/"+users[0].code+"/vnc.html?path=connect/"+users[0].code+"/websocketify&password=7b0ce21a0d8d3c7adec51d48abe2a3e9&autoconnect=true&reconnect=true&modelpath="+req.query.modelpath)
+					.then((response)=>{
+						if(response.status==200){
+							res.redirect("https://student.instances.modeller.cloud/connect/"+users[0].code+"/vnc.html?path=connect/"+users[0].code+"/websocketify&password=7b0ce21a0d8d3c7adec51d48abe2a3e9&autoconnect=true&reconnect=true&modelpath="+req.query.modelpath)
+						}else{
+							res.render("reload-container",{
+								bucket: bucket
+							})
+						}
+					})
+					.catch((error)=>{
+						res.render("reload-container",{
+							bucket: bucket
+						})
+					})
 				}else{
 					console.log(error)
 					res.render("message",{
@@ -250,7 +278,7 @@ server.get('/lmsLogin/:hostname/:lmsid',async (req,res)=>{
 					})
 				}
 				
-			})*/
+			})
 			
 		}else{
 			res.render("lmsLogin",{
